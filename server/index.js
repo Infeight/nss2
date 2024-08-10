fcb2b64239df4965d8c76972f0c0156632acf28d
Tarund777 <tarundandugula1@gmail.com>
2024-08-10 23:39:01 +0530

    netlify cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const { METHODS } = require('http');
 const app = express()
 
 const corsheader = {
-  origin: 'https://nss2-phi.vercel.app/',
+  origin: 'https://nssiiitdmkurnool.netlify.app/',
   METHODS:['GET','POST'],
   allowedHeaders:['Content-Type', 'Authorization'],
   credentials:true
