cc222072d042f774885c8aef961db8c3f2de9b14
Infeight <167733649+Infeight@users.noreply.github.com>
2024-08-12 01:03:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express()
 
 // app.use(cors({origin:'https://nssiiitdmkurnool.netlify.app/'}))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://example.com');
+  res.header('Access-Control-Allow-Origin', 'https://nssiiitdmkurnool.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
